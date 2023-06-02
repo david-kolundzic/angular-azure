d6a2fe62c40a1e7249842cbac60a19ce68cbc6d4
david-kolundzic <david-kolundzic@outlook.com>
2023-06-02 13:30:01 +0200

    Add test text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}}, </div> <div>test publish na Azure static web app</div>`,
 })
 export class AppComponent {
   value = 'World';
