6a71357d4dae4814532ded4ffbf9779b185aac24
david-kolundzic <david-kolundzic@outlook.com>
2023-06-05 16:51:34 +0200

    azure function message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
   <app-nav-menu></app-nav-menu>
+  <div>{{message}}</div>
   <app-player></app-player>`,
 })
 export class AppComponent {
+  message=''
   value = 'World';
+  constructor(private http: HttpClient){
+    this.http.get('/api/message').subscribe(
+    (resp: any) => this.message = resp.text );
+  }
 }
