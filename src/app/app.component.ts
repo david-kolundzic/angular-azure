91b10264b04e2f5c6fde96e963fcd0595af48f9f
david-kolundzic <david-kolundzic@outlook.com>
2023-06-06 08:29:13 +0200

    helloMessage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,32 @@ import { Component } from '@angular/core';
   template: `
   <app-nav-menu></app-nav-menu>
   <div>{{message}}</div>
+  <div></div>
   <app-player></app-player>`,
 })
 export class AppComponent {
-  message=''
+  message='';
+  messageHello='';
   value = 'World';
-  constructor(private http: HttpClient){
-    this.http.get('/api/message?name=david', {responseType:'text'}).subscribe({
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+
+    this.http.get('/api/message?name=david', { responseType:'text' }).subscribe({
       next: (resp: any) => {
         console.log("PORUKA");
-      console.log(resp)
-      this.message = resp.toString();
-    } });
+        console.log(resp)
+        this.message = resp.toString();
+      } 
+    });
+
+    this.http.get('/api/messageHello', { responseType: 'json' }).subscribe({
+      next: (resp: any) => {
+        console.log("PORUKA HELLO");
+        console.log(resp)
+        this.messageHello = resp.toString();
+      } 
+    });
+
   }
 }
